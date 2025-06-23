d3c0784107f12c1daac67944a0ece4d029ef33b7
sucharita-pal-04 <sucharitapal04@gmail.com>
2025-06-23 10:58:49 +0530

    Changes:Firebase Confogurations & Userdata Saved Successfully using Handlers

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,11 +1,14 @@
 import Header  from "@/components/header";
 import { Outlet } from "react-router-dom";
  import  Footer  from "@/components/footer";
+ import AuthHandler from "@/handlers/auth-handler";
 
 export const PublicLayout = () => {
   return (
     <div className="w-full">
       {/* handler to store the user data */}
+
+      <AuthHandler/>
        <Header />
 
       <Outlet />
